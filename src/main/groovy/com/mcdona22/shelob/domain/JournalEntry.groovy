d8a6791171467c82f9bec763b11c80d55cfff141
John Mac <mcdona22@gmail.com>
2016-01-18 14:02:59 +0000

    added some basic valdation rules using javax specificaiton

--- src/main/groovy/com/mcdona22/shelob/domain/JournalEntry.groovy
+++ src/main/groovy/com/mcdona22/shelob/domain/JournalEntry.groovy
@@ -1,7 +1,18 @@
 package com.mcdona22.shelob.domain
 
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+
 class JournalEntry {
+    int id
+    @NotNull(message = "the timestamp is mandatory")
     Date timestamp
+
+    @NotNull(message = "the service name is mandatory")
+    @Pattern(regexp = '^\\w.*', message = "you may not use the empty string")
     String serviceName
+
+    @NotNull(message = "the entry type is mandatory")
     EntryType entryType
 }
+
--- src/test/groovy/com/mcdona22/shelob/domain/JournalEntrySpec.groovy
+++ src/test/groovy/com/mcdona22/shelob/domain/JournalEntrySpec.groovy
@@ -1,8 +1,20 @@
 package com.mcdona22.shelob.domain
-
 import spock.lang.Specification
 
+import javax.validation.ConstraintViolation
+import javax.validation.Validation
+import javax.validation.Validator
+import javax.validation.ValidatorFactory
+
 class JournalEntrySpec extends Specification{
+
+    static Validator validator
+
+    def setupSpec(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory()
+        validator = factory.validator
+    }
+
 
     void "should behave correctly with the map constructor"(){
         setup:
@@ -13,5 +25,29 @@ class JournalEntrySpec extends Specification{
             entry.timestamp == map.timestamp
             entry.serviceName == map.serviceName
             entry.entryType == map.entryType
+            entry.id == 0   // default value
+    }
+
+    void "the object should fail validation correctly"(){
+        setup:
+            Map map = [timestamp: timestamp, serviceName: serviceName,entryType: entryType]
+            JournalEntry entry = new JournalEntry(map)
+        when:
+            Set<ConstraintViolation<JournalEntry>> violations = validator.validate(entry)
+        then:
+            println map
+            println violations
+            assert violations.size() == 1
+            assert violations[0]?.propertyPath.toString() == property
+            violations[0]?.message
+        where:
+        property        |timestamp  |serviceName    |entryType
+        "timestamp"     |null       |"test-service" | EntryType.Register
+        "serviceName"   |new Date() | null          | EntryType.Register
+        "serviceName"   |new Date() | "  "          | EntryType.Register
+        "serviceName"   |new Date() | ""            | EntryType.Register
+        "entryType"     |new Date() | "test-service"| null
+
+
     }
 }
