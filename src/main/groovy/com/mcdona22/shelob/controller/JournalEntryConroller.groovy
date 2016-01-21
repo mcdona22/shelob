815feb375ae60e356188abe2026704118eb69ae2
John Mac <mcdona22@gmail.com>
2016-01-21 18:14:15 +0000

    completed the wiring for the controller so that it can now respond to http POST request - happy path.  Still need to * test the repsonse object's values against expectations * look at the unhappy path with invalid entry details * not happy with the timestamp - dont think we need to be asking the client to provide that

--- src/main/groovy/com/mcdona22/shelob/controller/JournalEntryConroller.groovy
+++ src/main/groovy/com/mcdona22/shelob/controller/JournalEntryConroller.groovy
@@ -2,17 +2,24 @@ package com.mcdona22.shelob.controller
 
 import com.mcdona22.shelob.domain.JournalEntry
 import com.mcdona22.shelob.service.JournalEntryDao
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
-
 
 @RestController
 class JournalEntryController {
+    final static Logger LOG = LoggerFactory.getLogger(JournalEntryController)
     @Autowired JournalEntryDao dao
 
 
     // TODO make this a POST
-    Map saveUpdate(JournalEntry entry){
+    @RequestMapping( value = "/journalentry", method = RequestMethod.POST)
+    Map saveUpdate(@RequestBody JournalEntry entry){
+        LOG.info "about to write $entry"
         return dao.createJournalEntry(entry)
     }
 }
--- src/test/groovy/com/mcdona22/shelob/controller/JournalEntryControllerIntegrationSpec.groovy
+++ src/test/groovy/com/mcdona22/shelob/controller/JournalEntryControllerIntegrationSpec.groovy
@@ -1,19 +1,52 @@
 package com.mcdona22.shelob.controller
-
 import com.mcdona22.shelob.Application
+import com.mcdona22.shelob.domain.EntryType
+import groovy.json.JsonBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
 import spock.lang.Specification
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
 
 @SpringApplicationConfiguration(classes = Application)
-
 class JournalEntryControllerIntegrationSpec extends Specification{
     @Autowired JournalEntryController controller
+    MockMvc mvc
+    String path = "/journalentry"
 
+    def setup(){
+        mvc = standaloneSetup(controller ).build()
+
+    }
     def "wiring should be correct"(){
         expect:
             controller?.dao?.repository     // wired through the entire stack
+    }
+
+    def "well formed POST should successfully create a journal entry"(){
+        setup:
+            controller.dao.repository.deleteAll()
+            Map entry = [timestamp: new Date(), serviceName: 'ctl-test-service', entryType: EntryType.Register]
+            String json = new JsonBuilder(entry).toPrettyString()
+            println json
+        expect:
+            controller.dao.repository.findAll().size() == 0
+        when:
+            MvcResult result = mvc.perform(post(path)
+                                .content(json)
+                                .header("Content-Type", "application/json"))
+                            .andExpect(status().isOk())
+                            .andExpect(content().contentType("application/json;charset=UTF-8"))
+                            .andDo(print())
+                            .andReturn()
+        then:
+            result.response.contentAsString
     }
 
 }
